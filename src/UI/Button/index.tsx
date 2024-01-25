fc643f97a215143780810ec3a30eb94449b9503a
Everton Afonso <evertonafonso0@gmail.com>
2024-01-25 20:52:43 -0300

    feat(Button): button component improvements

--- src/UI/Button/index.tsx
+++ src/UI/Button/index.tsx
@@ -1,3 +1,5 @@
+import { ButtonHTMLAttributes } from 'react'
+
 import * as S from './styles'
 
 export type ButtonColors = 'blue' | 'green' | 'gray'
@@ -5,10 +7,15 @@ export type ButtonColors = 'blue' | 'green' | 'gray'
 type ButtonProps = {
   title: string
   color?: ButtonColors
-}
+  as?: React.ElementType
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button = ({ title, color = 'blue' }: ButtonProps) => {
-  return <S.WrapperButton $color={color}>{title}</S.WrapperButton>
+const Button = ({ title, color = 'blue', ...props }: ButtonProps) => {
+  return (
+    <S.WrapperButton {...props} $color={color}>
+      {title}
+    </S.WrapperButton>
+  )
 }
 
 export default Button
